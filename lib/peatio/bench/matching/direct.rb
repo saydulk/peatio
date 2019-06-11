ffa2d5aa011c91ca6251e3cf7ffdf655a1ccd808
mnaichuk <mnaichuk@heliostech.fr>
2019-06-11 12:45:23 +0300

    Update Peatio::Bench with matching engine bug

--- lib/peatio/bench/matching/direct.rb
+++ lib/peatio/bench/matching/direct.rb
@@ -40,7 +40,27 @@ module Bench
       def process_messages
         loop do
           order = @injector.pop
-          break unless order
+          if order.present?
+            order.fix_number_precision
+            order.locked = order.origin_locked = order.compute_locked
+            order.hold_account!.lock_funds(order.locked)
+            order.save!
+          end
+          if @injector.size == 0
+            10.times { p "-" }
+            sleep @orders_number/10 + 1
+            pp "Market Orders: #{Order.where(ord_type: :market).count} "
+            min_ask = OrderAsk.where(state: :wait).pluck(:price).compact.min
+            max_bid = OrderBid.where(state: :wait).pluck(:price).compact.max
+            if min_ask.present? && max_bid.present? && max_bid >= min_ask
+              10.times { p "-" }
+              p 'Wrong mathing behaviour'
+              pp " Min ASK #{min_ask}"
+              pp " Max BID #{max_bid}"
+              10.times { p "-" }
+            end
+            break
+          end
           @matching.process({action: 'submit', order: order.to_matching_attributes}, 'metadata', 'delivery_info')
         rescue StandardError => e
           Kernel.puts e
